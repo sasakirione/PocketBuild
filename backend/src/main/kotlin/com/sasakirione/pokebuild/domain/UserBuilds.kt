40e7d233f9b9fd4bc49dbb6e90643939f765cdd5
sasakirione <sasakirione@gmail.com>
2023-04-26 23:33:41 +0900

    Doc: ViewModel系にコメント

--- backend/src/main/kotlin/com/sasakirione/pokebuild/domain/UserBuilds.kt
+++ backend/src/main/kotlin/com/sasakirione/pokebuild/domain/UserBuilds.kt
@@ -1,17 +1,46 @@
 package com.sasakirione.pokebuild.domain
 
-import com.fasterxml.jackson.databind.ObjectWriter.GeneratorSettings
 
+/**
+ * ユーザーの構築
+ */
 data class UserBuild(
+    /**
+     * 構築ID
+     */
     val id: Int,
+    /**
+     * ユーザーID
+     */
     val userId: Int,
+    /**
+     * 構築名
+     */
     val name: String,
+    /**
+     * 構築の説明
+     */
     val description: String,
+    /**
+     * シリーズ
+     */
     val series: Int,
+    /**
+     * 世代
+     */
     val generation: Int,
 )
 
+/**
+ * ユーザーの構築に登録されているポケモンのリスト
+ */
 data class UserBuildPokemonList(
+    /**
+     * 構築ID
+     */
     val buildId: Int,
+    /**
+     * ポケモンのリスト
+     */
     val pokemonList: List<UserPokemon>
 )
--- backend/src/main/kotlin/com/sasakirione/pokebuild/domain/UserPokemon.kt
+++ backend/src/main/kotlin/com/sasakirione/pokebuild/domain/UserPokemon.kt
@@ -1,25 +1,85 @@
 package com.sasakirione.pokebuild.domain
 
+/**
+ * ユーザーのポケモン
+ */
 data class UserPokemon(
+    /**
+     * ポケモンID
+     */
     val id: Int,
+    /**
+     * ユーザーID
+     */
     val userId: Int,
+    /**
+     * ポケモンの種族ID
+     */
     val pokemonId: Int,
+    /**
+     * ポケモンの努力値
+     */
     val ev: UserPokemonValue,
+    /**
+     * ポケモンの個体値
+     */
     val iv: UserPokemonValue,
+    /**
+     * ポケモンの性格
+     */
     val nature: Int,
+    /**
+     * ポケモンの特性
+     */
     val ability: Int,
+    /**
+     * ポケモンの技
+     */
     val moves: List<Int>,
+    /**
+     * ポケモンのタグ
+     */
     val tags: List<Int>,
+    /**
+     * ポケモンの道具
+     */
     val good: Int,
+    /**
+     * ポケモンのニックネーム
+     */
     val nickName: String,
+    /**
+     * ポケモンのテラスタイプ
+     */
     val terasType: Int
 )
 
+/**
+ * ユーザーのポケモンの値
+ */
 data class UserPokemonValue(
+    /**
+     * HP
+     */
     val h: Int,
+    /**
+     * 攻撃
+     */
     val a: Int,
+    /**
+     * 防御
+     */
     val b: Int,
+    /**
+     * 特攻
+     */
     val c: Int,
+    /**
+     * 特防
+     */
     val d: Int,
+    /**
+     * 素早さ
+     */
     val s: Int
 )
